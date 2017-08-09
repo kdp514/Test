487bf146805b74ce42af5a66c07e4922429b9686
kdp514 <30856253+kdp514@users.noreply.github.com>
2017-08-09 15:32:33 +0900

    테스트
    테스트

---
+++ test.java
@@ -0,0 +1,14 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20;
+		
+		System.out.println(Math.sqrt(a));
+		
+		System.out.println(Math.sin(a));
+	}
+
+}
